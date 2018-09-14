1cd24449d1023827dac52ce19e32ab605c97eef9
TimG <timgm3@gmail.com>
2018-09-13 21:02:09 -0700

    file structure and first liri.js requires

---
+++ liri.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+var spotifyKeys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(spotifyKeys.spotify);
+
+
+switch(process.argv[2]){
+    case "concert-this":
+    
+    break;
+
+
+
+
+
+
+
+};
